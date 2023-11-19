b51c266e5555ada9677de7fd7696efab9ca3b6d4
Rehan Jayathilaka <blastburn750@gmail.com>
2023-11-19 17:01:53 -0600

    Started writing the time function

---
+++ timefunc.py
@@ -0,0 +1,13 @@
+import datetime
+
+
+#run this if a ZID swiped today was swiped again
+def GetTimeDif(ZID):
+    #look in the file for when he swiped in 
+    #substract 
+
+
+    
+
+current_time = datetime.datetime.now()
+print(current_time)
