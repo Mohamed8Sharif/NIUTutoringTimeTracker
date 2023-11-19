9e4f8d9e3d449163c50fddab6c5a333d10784288
Mohamed Sharif <mohamed8sharif@gmail.com>
2023-11-19 17:11:16 -0600

    Add basic interface to be modified

--- timefunc.py
+++ timefunc.py
@@ -1,13 +1,30 @@
 import datetime
+import wx
+
 
 
 #run this if a ZID swiped today was swiped again
 def GetTimeDif(ZID):
     #look in the file for when he swiped in 
     #substract 
+    current_time = datetime.datetime.now()
+    print(current_time)
+
 
 
     
 
-current_time = datetime.datetime.now()
-print(current_time)
+class MyFrame(wx.Frame):    
+    def __init__(self):
+        super().__init__(parent=None, title='Hello World')
+        panel = wx.Panel(self)
+
+        self.text_ctrl = wx.TextCtrl(panel, pos=(5, 5))
+        my_btn = wx.Button(panel, label='Press Me', pos=(5, 55))
+
+        self.Show()
+
+if __name__ == '__main__':
+    app = wx.App()
+    frame = MyFrame()
+    app.MainLoop()
