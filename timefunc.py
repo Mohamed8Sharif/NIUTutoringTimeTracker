50e2ad63b0874d3a4e005f55f171bed7d4e7eea2
Mohamed Sharif <mohamed8sharif@gmail.com>
2023-11-19 20:07:59 -0600

    Connect textbox to record writing function

--- timefunc.py
+++ timefunc.py
@@ -2,6 +2,13 @@ import datetime
 import wx
 import pandas as pd
 
+# main function
+def main():
+    app = wx.App()
+    frame = MyFrame()
+    app.MainLoop()
+    #ZID to be pulled from swipe
+    GetTimeDif("Z1980556")
 
 
 #run this if a ZID swiped today was swiped again
@@ -14,12 +21,9 @@ def GetTimeDif(zid):
     df.loc[df["ZID"] == zid , "Time out"] =  current_time
     
     #look in the file for when he swiped in 
-    
 
     current_time = datetime.datetime.now().strftime("%H:%M %A")
 
-
-      
     df.to_csv('test.csv', index=False)
     
     
@@ -28,23 +32,28 @@ def GetTimeDif(zid):
     
     #print(current_time)
 
-
-
-    
-
+# GUI for the application
 class MyFrame(wx.Frame):    
     def __init__(self):
         super().__init__(parent=None, title='Hello World')
-        panel = wx.Panel(self)
-
-        self.text_ctrl = wx.TextCtrl(panel, pos=(5, 5))
-        my_btn = wx.Button(panel, label='Press Me', pos=(5, 55))
-
+        panel = wx.Panel(self)        
+        my_sizer = wx.BoxSizer(wx.VERTICAL)        
+        self.text_ctrl = wx.TextCtrl(panel)
+        my_sizer.Add(self.text_ctrl, 0, wx.ALL | wx.CENTER, 5)        
+        my_btn = wx.Button(panel, label='Press Me')
+        my_btn.Bind(wx.EVT_BUTTON, self.on_press)
+        my_sizer.Add(my_btn, 0, wx.ALL | wx.CENTER, 5)        
+        panel.SetSizer(my_sizer)        
         self.Show()
 
+    def on_press(self, event):
+        value = self.text_ctrl.GetValue()
+        if not value:
+            print("Error I guess")
+        else:
+            GetTimeDif(value)
+
+
+# Calls main function
 if __name__ == '__main__':
-    app = wx.App()
-    frame = MyFrame()
-    app.MainLoop()
-    #ZID to be pulled from swipe
-    GetTimeDif("Z1980556")
+    main()
