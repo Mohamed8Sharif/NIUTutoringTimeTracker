54a3fa254238a1a79d5b5d1b0b7d32cf821b9f8a
Mohamed Sharif <mohamed8sharif@gmail.com>
2023-12-21 15:15:31 -0600

    Create a menu bar

--- timefunc.py
+++ timefunc.py
@@ -44,7 +44,8 @@ class MyFrame(wx.Frame):
         my_btn.Bind(wx.EVT_BUTTON, self.on_press)
         my_sizer.Add(my_btn, 0, wx.ALL | wx.CENTER, 5)        
         panel.SetSizer(my_sizer)
-        now = wx.DateTime.Now()        
+        now = wx.DateTime.Now()
+        self.create_menu()        
         self.Show()
 
     def on_press(self, event):
@@ -54,6 +55,29 @@ class MyFrame(wx.Frame):
         else:
             # GetTimeDif(value)
             print("no")
+
+    def create_menu(self):
+        menu_bar = wx.MenuBar()
+        file_menu = wx.Menu()
+        open_folder_menu_item = file_menu.Append(
+            wx.ID_ANY, 'Open Folder', 
+            'Open a folder with MP3s'
+        )
+        menu_bar.Append(file_menu, '&File')
+        self.Bind(
+            event=wx.EVT_MENU, 
+            handler=self.on_open_folder,
+            source=open_folder_menu_item,
+        )
+        self.SetMenuBar(menu_bar)
+
+    def on_open_folder(self, event):
+        title = "Choose a directory:"
+        dlg = wx.DirDialog(self, title, 
+                           style=wx.DD_DEFAULT_STYLE)
+        if dlg.ShowModal() == wx.ID_OK:
+            self.panel.update_mp3_listing(dlg.GetPath())
+        dlg.Destroy()
 
 
 # Calls main function
